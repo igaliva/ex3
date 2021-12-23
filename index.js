f0fb80fff3a3d5d1a5ba636e6a5647633b1d2452
igaliva <96495282+igaliva@users.noreply.github.com>
2021-12-23 22:20:04 +0200

    ex written

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+let num1 = prompt('please enter the first number');
+let num2 = prompt('please enter the second number');
+let num3 = parseInt(num1)/parseInt(num2);
+alert('The result of the division between '+num1+' and '+num2+' is '+(num3).toFixed(2)+'.');
